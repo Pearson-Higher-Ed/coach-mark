9c1ae6df4324224386bfb6fa375a30dcf17880aa
mallory <stommepoes@stommepoes.nl>
2016-01-05 20:19:12 +0100

    merged fixes

--- src/js/CoachMark.js
+++ src/js/CoachMark.js
@@ -49,10 +49,6 @@ export default class CoachMark {
 		closeSpan.setAttribute('aria-hidden','true');
 		close.appendChild(closeSpan);
 
-		closeSpan[internalText] = '✕';
-		closeSpan.setAttribute('aria-hidden','true');
-		close.appendChild(closeSpan);
-
 		container.className = 'o-coach-mark__container';
 		container.style.visibility = 'hidden';
 		container.style.display = 'block';
