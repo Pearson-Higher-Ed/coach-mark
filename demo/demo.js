0dfcfdb51393a7541eb2b75122d13f378b05a240
Brian Dunn <brian.dunn@pearson.com>
2018-02-08 16:48:58 -0700

    chore: delete commented code

--- demo/demo.js
+++ demo/demo.js
@@ -1,27 +1,5 @@
 import CoachMark from '../index';
 
-// import { addLocaleData, IntlProvider } from 'react-intl';
-//
-// // Import Translations...
-// import frJson       from './translations/fr.json';
-// import frLocaleData from 'react-intl/locale-data/fr';
-//
-// import enUSJson       from './translations/en-US.json';
-// import enUSLocaleData from 'react-intl/locale-data/en';
-//
-// // Associate Language Abbreviation with json filename...
-// const translations = {
-//   'fr'    : frJson,
-//   'en-US' : enUSJson
-// };
-//
-// // Add Language
-// addLocaleData(frLocaleData);
-// addLocaleData(enUSLocaleData);
-//
-// // Determining the User's Locale
-// const locale = (navigator.language) ? navigator.language : navigator.browserLanguage;
-
 export const showFirst = () => {
   new CoachMark({
     elementId: 'demo-target1',
