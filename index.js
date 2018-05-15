8f3d2d7925f94484e3c7706ed54f0c5500c9cd1d
David O'Dey <dodey@icloud.com>
2018-05-15 15:33:32 -0700

    all buttons, except the ones inside the coachmark get disabled until the coachmark closes

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import ComponentOwner from './src/js/component-owner';
 
 export default class CoachMark {
-  
+
   constructor(config) {
     this.config = config;
     const target = document.getElementById(config.elementId);
     this.container = document.createElement('div');
     target.parentNode.insertBefore(this.container, target.nextSibling);
-    
+
     ReactDOM.render(
       <ComponentOwner
         target={target}
@@ -19,8 +19,15 @@ export default class CoachMark {
       this.container
     );
   }
-  
+
   removeCoachMark = (event) => {
+    if (!('remove' in Element.prototype)) {
+      Element.prototype.remove = function() {
+        if (this.parentNode) {
+          this.parentNode.removeChild(this);
+        }
+      };
+    }
     ReactDOM.unmountComponentAtNode(this.container);
     this.container.remove();
     if (this.config.callback) {
